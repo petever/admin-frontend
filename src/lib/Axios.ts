a7b5dbfeac88df1cf5f321b362c098e2e21d453a
김승훈 <rlatmdgns94@naver.com>
2022-02-08 02:08:22 +0900

    feat: axios withCredentials 설정

--- src/lib/Axios.ts
+++ src/lib/Axios.ts
@@ -2,9 +2,10 @@ import axios, { AxiosInstance } from "axios";
 
 const Axios: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // 기본 서버 주소 입력
-  // headers: {
-  //   access_token: cookies.get("access_token"),
-  // },
+  headers: {
+    "Access-Control-Allow-Origin": "https://api.petever.pet",
+  },
+  withCredentials: true,
 });
 
 export default Axios;
